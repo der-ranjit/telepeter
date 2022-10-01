66cdf3b5ac55b1b81ad0eece1328fcaf32acc5af
marverati <marverati@gmail.com>
2022-10-01 23:44:37 +0200

    Add basic Spikes script

---
+++ Assets/Scripts/Spikes.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spikes : MonoBehaviour
+{
+    public void OnTriggerEnter2D(Collider2D other) {
+        if (other.gameObject.tag == "Player") {
+            Debug.Log("kill!!!!");
+        }
+    }
+}
